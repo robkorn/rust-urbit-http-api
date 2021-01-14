a589b6e07f96c905c524cf84aab7037fec064faa
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2021-01-14 08:41:45 +0100

    Added wrapper func for simplifying getting da time

--- src/helper.rs
+++ src/helper.rs
@@ -1,10 +1,21 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
 // `@ud` ~1970.1.1
 static DA_UNIX_EPOCH: u128 = 170141184475152167957503069145530368000;
 // `@ud` ~s1
 static DA_SECOND: u128 = 18446744073709551616;
 
-// Convert from Unix time to Urbit `@da` time
+// Convert from Unix time in milliseconds to Urbit `@da` time
 pub fn unix_time_to_da(unix_time: u64) -> u128 {
     let time_since_epoch = (unix_time as u128 * DA_SECOND) / 1000;
     DA_UNIX_EPOCH + time_since_epoch
+}
+
+// Acquire the current time in Urbit `@da` encoding
+pub fn get_current_da_time() -> u128 {
+    let unix_time = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_millis() as u64;
+    unix_time_to_da(unix_time)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ pub mod subscription;
 
 pub use channel::Channel;
 pub use error::{Result, UrbitAPIError};
+pub use helper::get_current_da_time;
 pub use interface::ShipInterface;
 pub use local_config::{create_new_ship_config_file, ship_interface_from_local_config};
 pub use subscription::Subscription;
