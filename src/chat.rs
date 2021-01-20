43e6b66ec61a48b05b3f672d053b78216952aea9
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2021-01-20 21:13:43 +0100

    Chat send_message() now uses GraphStore post()

--- src/chat.rs
+++ src/chat.rs
@@ -1,7 +1,5 @@
-use crate::helper::get_current_da_time;
-use crate::{Channel, Result, UrbitAPIError};
+use crate::{Channel, Result};
 use json::{object, JsonValue};
-use std::time::{SystemTime, UNIX_EPOCH};
 
 pub struct Chat<'a> {
     pub channel: &'a mut Channel,
@@ -14,54 +12,12 @@ impl<'a> Chat<'a> {
         chat_name: &str,
         message: &JsonValue,
     ) -> Result<()> {
-        // Add the ~ to the ship name to be used within the poke json
-        let ship = format!("~{}", self.channel.ship_interface.ship_name);
+        let contents = vec![object! {
+            "text": message.clone()
+        }];
 
-        // Get the current Unix Time
-        let unix_time = SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap()
-            .as_millis() as u64;
-
-        // The index. For chat this is the `now` in Urbit as an atom.
-        // Need to implement properly with `now` still.
-        let index = format!("/{}", get_current_da_time());
-
-        // Creating the json by adding the index dynamically for the key
-        // for the inner part of the json
-        let mut nodes_json = object!();
-        nodes_json[index.clone()] = object! {
-                        "post": {
-                            "author": ship.clone(),
-                            "index": index.clone(),
-                            "time-sent": unix_time,
-                            "contents": [{
-                                "text": message.clone()
-                            }],
-                            "hash": null,
-                            "signatures": []
-                        },
-                        "children": null
-        };
-        // Rest of the json creation
-        let poke_json = object! {
-            "add-nodes": {
-                "resource": {
-                    "ship": chat_ship,
-                    "name": chat_name
-                },
-            "nodes": nodes_json
-            }
-        };
-
-        let resp = (&mut self.channel).poke("graph-push-hook", "graph-update", &poke_json)?;
-
-        if resp.status().as_u16() == 204 {
-            Ok(())
-        } else {
-            return Err(UrbitAPIError::FailedToSendChatMessage(
-                chat_name.to_string(),
-            ));
-        }
+        self.channel
+            .graph_store()
+            .post(chat_ship, chat_name, contents)
     }
 }
