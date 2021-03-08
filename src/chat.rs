e834dbc72683ba0f8f1e69e6de4059b8a608369d
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2021-03-08 20:51:11 +0100

    Wrapped messaging functionality for chat

--- src/chat.rs
+++ src/chat.rs
@@ -1,6 +1,7 @@
 use crate::error::Result;
 use crate::traits::messaging::{AuthoredMessage, Message, Messaging};
-use crate::{Channel, Node};
+use crate::Channel;
+use crossbeam::channel::Receiver;
 
 /// A struct that provides an interface for interacting with Urbit chats
 pub struct Chat<'a> {
@@ -23,5 +24,33 @@ impl<'a> Chat<'a> {
         message: &Message,
     ) -> Result<String> {
         self.send_message(chat_ship, chat_name, message)
+    }
+
+    /// Extracts chat log automatically into a list of formatted `String`s
+    pub fn export_chat_log(&mut self, chat_ship: &str, chat_name: &str) -> Result<Vec<String>> {
+        self.export_message_log(chat_ship, chat_name)
+    }
+
+    pub fn export_chat_authored_messages(
+        &mut self,
+        chat_ship: &str,
+        chat_name: &str,
+    ) -> Result<Vec<AuthoredMessage>> {
+        self.export_authored_messages(chat_ship, chat_name)
+    }
+
+    /// Subscribe to and watch for messages. This method returns a `Receiver` with the
+    /// `AuthoredMessage`s that are posted after subscribing. Simply call `receiver.try_recv()`
+    /// to read the next `AuthoredMessage` if one has been posted.
+    ///
+    /// Technical Note: This method actually creates a new `Channel` with your Urbit Ship, and spawns a new unix thread
+    /// locally that processes all messages on said channel. This is required due to borrowing mechanisms in Rust, however
+    /// on the plus side this makes it potentially more performant by each subscription having it's own unix thread.
+    pub fn subscribe_to_chat(
+        &mut self,
+        chat_ship: &str,
+        chat_name: &str,
+    ) -> Result<Receiver<AuthoredMessage>> {
+        self.subscribe_to_messages(chat_ship, chat_name)
     }
 }
