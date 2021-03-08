86aefd03ba9d85c9ea57a236fceddc89b6e885f5
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2021-03-08 20:34:11 +0100

    Boilerplate for hark-store & invite-store

---
+++ src/harkstore.rs
@@ -0,0 +1,12 @@
+use crate::{Channel, Node, Result, UrbitAPIError};
+use crossbeam::channel::{unbounded, Receiver};
+use json::JsonValue;
+use std::thread;
+use std::time::Duration;
+
+/// A struct that provides an interface for interacting with hark-store
+pub struct HarkStore<'a> {
+    pub channel: &'a mut Channel,
+}
+
+impl HarkStore {}
---
+++ src/invitestore.rs
@@ -0,0 +1,21 @@
+use crate::{Channel, Node, Result, UrbitAPIError};
+use crossbeam::channel::{unbounded, Receiver};
+use json::JsonValue;
+use std::thread;
+use std::time::Duration;
+
+/// A struct that provides an interface for interacting with invite-store
+pub struct InviteStore<'a> {
+    pub channel: &'a mut Channel,
+}
+
+impl InviteStore {
+    pub fn accept_invite(&self, term: &str, uid: &str) {
+        // let mut poke2_data = json::JsonValue::new_object();
+        // poke2_data["accept"] = json::JsonValue::new_object();
+        // poke2_data["accept"]["term"] = "graph".to_string().into();
+        // poke2_data["accept"]["uid"] = poke_channel.uid.clone().into();
+        // let _poke2_response = poke_channel.poke("invite-store", "invite-action", &poke_data);
+        todo!();
+    }
+}
